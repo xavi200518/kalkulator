dfeab3872bae923f9e4a16c1194d4e86118e2079
Enej Kacijan <enej.kacijan@gmail.com>
2025-10-18 18:28:04 +0200

    Initial commit: basic calculator

---
+++ README.md
@@ -0,0 +1,7 @@
+# C++ Command-Line Calculator
+
+A simple calculator written in C++ for testing Git functionality.
+
+## Build
+```bash
+g++ -std=c++17 -o calculator src/main.cpp
---
+++ src/calculator.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    char oper;
+    double result;
+
+    cout << "Vnesite prvo celo število: ";
+    cin >> num1;
+    cout << "Vnesite operator (+, -, x, /): ";
+    cin >> oper;
+    cout << "Vnesite drugo celo število: ";
+    cin >> num2;
+
+    switch (oper) {
+        case '+':
+            result = num1 + num2;
+            cout << "Rezultat: " << result << endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "Rezultat: " << result << endl;
+            break;
+        case 'x':
+        case '*':
+            result = num1 * num2;
+            cout << "Rezultat: " << result << endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = static_cast<double>(num1) / num2;
+                cout << "Rezultat: " << result << endl;
+            } else {
+                cout << "Napaka: Deljenje z 0 ni dovoljeno!" << endl;
+            }
+            break;
+        default:
+            cout << "Napaka: Neveljaven operator!" << endl;
+    }
+
+    return 0;
+}
+
